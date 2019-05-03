993f253cc336fdbe9ffa8edbf962f950c16fdf3d
Saumya Batra <twinkle.batra1424@gmail.com>
2019-05-03 07:47:30 +0200

    Add files via upload

---
+++ floatToString.java
@@ -0,0 +1,20 @@
+package newPackage;
+
+class floatToString {
+
+	public static void main(String[] args) {
+		String s1="10";
+		Float f=Float.valueOf(s1);//string to Float
+		System.out.println(f);
+		float g=(float) 1.0;
+		Float f2=Float.valueOf(g);//float to Float
+		System.out.println(f2);
+		String s2=String.valueOf(g);//float to string
+		System.out.println(s2);
+		
+		
+		
+
+	}
+
+}
